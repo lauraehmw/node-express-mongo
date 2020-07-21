027be28959b0e34fc4fd8b5aae43c523ee2ea208
--replace-all <--replace-all>
2020-07-21 15:46:58 -0500

    Serving static files

--- node-http/index.js
+++ node-http/index.js
@@ -1,16 +1,47 @@
 const  http = require('http');
+const fs = require('fs');
+const path = require('path')
 
 const hostname = 'localhost';
-
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
+    console.log("Request for "+req.url+" by method "+req.method);
 
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
+    if(req.method == 'GET'){
+        var fileURL;
+        if (req.url == '/') fileURL = 'index.html'
+        else fileURL = req.url
 
-    res.end('<html><body><h1>Hello, Baby moon!</h1></body></html>')
+        var filePath = path.resolve('./public/'+fileURL)
+        const fileExt = path.extname(filePath);
+        
+        if(fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+                if(!exists){
+                    res.statusCode = 404;
+                    res.setHeader('Content-type', 'text/html');
+                    res.end(`<html><body><h1>EROR 4040 ${fileURL} not found</h1></body></html>`)
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+
+        }else{
+            res.statusCode = 404;
+            res.setHeader('Content-type', 'text/html');
+            res.end(`<html><body><h1>EROR 4040 ${fileURL} not an HTML file</h1></body></html>`)
+            return;
+        }
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-type', 'text/html');
+        res.end(`<html><body><h1>EROR 4040 ${req.method} not supported</h1></body></html>`)
+        return;
+    }
 })
 
 server.listen(port, hostname, () =>{
